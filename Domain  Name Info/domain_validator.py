41fba4580bde2aac644c825104589047cbff04d9
Aditya Singh <6390adityasingh@gmail.com>
2025-09-16 14:11:40 +0530

    Domain Info Gathering

--- Domain  Name Info/domain_validator.py
+++ Domain  Name Info/domain_validator.py
@@ -13,18 +13,3 @@ if __name__ == "__main__":
         print(i," ",is_registered(i))
 
 
-
-
-# import whois
-
-# def is_registered(domain_name):
-#     try:
-#         w=whois.whois(domain_name)
-#     except Exception :
-#         return "Not registered"
-#     else:
-#         return "Registered"
-
-# if __name__=='__main__':
-#     print(is_registered('www.youtube.com'))
-#     print(is_registered('www.something-that-do-nt-exist.com'))
